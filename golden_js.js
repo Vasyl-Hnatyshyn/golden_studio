7796e6a4f4ae9817b3c5ca80437b1e81a6c6588f
Vasyl Hnatyshyn <46223913+gnatV7077@users.noreply.github.com>
2019-12-17 20:15:53 +0200

    firs_version

---
+++ golden_js.js
@@ -0,0 +1,33 @@
+﻿//Scrool element nav
+
+$(document).ready(function() {
+            $("a[href*='#']").on("click", function(e) {
+                var anchor = $(this);
+                $('html, body').stop().animate({
+                    scrollTop: $(anchor.attr('href')).offset().top
+                }, 777);
+                e.preventDefault();
+                return false;
+            });
+        });
+
+
+//button top
+
+
+
+
+let t;
+function up() {
+	let top = Math.max(document.body.scrollTop,document.documentElement.scrollTop);
+	if(top > 0) {
+		window.scrollBy(0,-100);
+		t = setTimeout('up()',20);
+	} else clearTimeout(t);
+	return false;
+}
+
+
+
+
+
