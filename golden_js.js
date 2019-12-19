ee62de5cadb3e46cb058ddc1821e8d46e1796278
Vasyl Hnatyshyn <46223913+gnatV7077@users.noreply.github.com>
2019-12-19 13:22:30 +0200

    _mobile

--- golden_js.js
+++ golden_js.js
@@ -1,15 +1,15 @@
 ﻿//Scrool element nav
 
-$(document).ready(function() {
-            $("a[href*='#']").on("click", function(e) {
-                var anchor = $(this);
-                $('html, body').stop().animate({
-                    scrollTop: $(anchor.attr('href')).offset().top
-                }, 777);
-                e.preventDefault();
-                return false;
-            });
-        });
+$(document).ready(function () {
+    $("a[href*='#']").on("click", function (e) {
+        var anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: $(anchor.attr('href')).offset().top
+        }, 777);
+        e.preventDefault();
+        return false;
+    });
+});
 
 
 //button top
@@ -19,15 +19,10 @@ $(document).ready(function() {
 
 let t;
 function up() {
-	let top = Math.max(document.body.scrollTop,document.documentElement.scrollTop);
-	if(top > 0) {
-		window.scrollBy(0,-100);
-		t = setTimeout('up()',20);
-	} else clearTimeout(t);
-	return false;
+    let top = Math.max(document.body.scrollTop, document.documentElement.scrollTop);
+    if (top > 0) {
+        window.scrollBy(0, -100);
+        t = setTimeout('up()', 20);
+    } else clearTimeout(t);
+    return false;
 }
-
-
-
-
-
